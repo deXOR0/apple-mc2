4f65082d56c77ddda3bd925aaeeab919e146bfa6
m salman a <mohammadalfarisi6@gmail.com>
2022-06-20 11:03:57 +0700

    add: CustomColors for Messaging UI

---
+++ comittea/Theme/CustomColors.swift
@@ -0,0 +1,17 @@
+//
+//  Theme.swift
+//  comittea
+//
+//  Created by Mohammad Alfarisi on 20/06/22.
+//
+
+import Foundation
+import UIKit
+
+struct CustomColors {
+    static let lightOrange: UIColor = #colorLiteral(red: 1, green: 0.7725490196, blue: 0.4431372549, alpha: 1)
+    static let blue: UIColor = #colorLiteral(red: 0.2705882353, green: 0.4, blue: 0.8352941176, alpha: 1)
+    static let gray: UIColor = #colorLiteral(red: 0.9058823529, green: 0.9058823529, blue: 0.9058823529, alpha: 1)
+    static let green: UIColor = #colorLiteral(red: 0.3764705882, green: 0.8862745098, blue: 0.1960784314, alpha: 1)
+    static let red: UIColor = #colorLiteral(red: 1, green: 0.462745098, blue: 0.462745098, alpha: 1)
+}
