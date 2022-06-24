f60433932ea67e0d96d617d3ca1035d8d3ddd9a3
Atyanta Awesa <awesaatyanta.p@gmail.com>
2022-06-24 10:14:54 +0700

    Fixed missing assets and standardized naming convention

--- comittea/Model/StaticStoriesData.swift
+++ comittea/Model/StaticStoriesData.swift
@@ -22,17 +22,17 @@ class StaticStoriesData {
               What a boring weekend Benny is having right now.
               Just sitting and doing nothing. Suddenly it hits him.
               He could go to the movies with his friends.
-            """, "book", "Story_Background_Movie", [
-            Chapter("Planning Ahead", "Chap_Planning Ahead", [Message]()),
-            Chapter("Buying Tickets", "Chap_Buying Tickets", [Message]()),
-            Chapter("Seating Placement", "Chap_Seating Placement", [Message]())
+            """, "Story_Logo_Movie", "Story_Background_Movie", [
+            Chapter("Planning Ahead", "Chapter_Planning Ahead", [Message]()),
+            Chapter("Buying Tickets", "Chapter_Buying Tickets", [Message]()),
+            Chapter("Seating Placement", "Chapter_Seating Placement", [Message]())
         ]),
-        Story("The Secretary", "The mayor is a very busy man, he has a lot of responsibilities. As time goes on, his workload seems to keep increasing. He decided that he needed help and hired you as his secretary. As a secretary, you're responsible for managing the mayor's schedule, setting up meetings, retrieving documents, etc.", "movie", "Story_Background_The Secretary", [
-            Chapter("Find Documents", "Chap_Documents", [Message]()),
-            Chapter("Must Go Faster!", "Chap_Faster", [Message]()),
-            Chapter("Sort Problems", "Chap_Problems", [Message]())
+        Story("The Secretary", "The mayor is a very busy man, he has a lot of responsibilities. As time goes on, his workload seems to keep increasing. He decided that he needed help and hired you as his secretary. As a secretary, you're responsible for managing the mayor's schedule, setting up meetings, retrieving documents, etc.", "Story_Logo_Secretary", "Story_Background_The Secretary", [
+            Chapter("Find Documents", "Chapter_Documents", [Message]()),
+            Chapter("Must Go Faster!", "Chapter_Faster", [Message]()),
+            Chapter("Sort Problems", "Chapter_Problems", [Message]())
         ]),
-        Story("Supermarket", "", "supermarket", "Story_Background_Supermarket", [
+        Story("Supermarket", "", "Story_Logo_Supermarket", "Story_Background_Supermarket", [
             Chapter("Monthly Groceries", "Chapter_Monthly", [Message]()),
             Chapter("Find the Banana", "Chapter_Banana", [Message]()),
             Chapter("Pay Lane", "Chapter_PayLine", [Message]())
